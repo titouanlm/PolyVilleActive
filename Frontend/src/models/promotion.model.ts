a866e025ffbe76f241ec695f67b6be62c7dd2aa6
abadina <50300952+abadina@users.noreply.github.com>
2020-12-11 16:58:46 +0100

    Ajout de promotion.model.ts

---
+++ Frontend/src/models/promotion.model.ts
@@ -0,0 +1,11 @@
+export interface promotion {
+  titre: string,
+  description: string;
+  date_debut: string;
+  date_fin: string;
+  notification_id: number;
+  event_id: number;
+  condition_id: number;
+  promo_id: number;
+  shop_id: number;
+}
