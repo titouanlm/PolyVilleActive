873017a7ab38f05ae8660a1bb8c106444b02eda4
Denisko185 <55317671+Denisko185@users.noreply.github.com>
2020-12-16 15:58:24 +0100

    Ajout des données d'acteurs de vie culturelle et d'employé de mairie #52

--- Backend/app/models/CulturalActor.model.js
+++ Backend/app/models/CulturalActor.model.js
@@ -2,6 +2,6 @@ const Joi = require('joi');
 const BaseModel = require('../utils/base-model.js');
 
 module.exports = new BaseModel('CulturalActor', {
-    Nom : Joi.toString(),
-    Prenom: Joi.toString()
+    nom : Joi.string(),
+    prenom: Joi.string()
 });
---
+++ Frontend/src/models/townHallEmployee.model.ts
@@ -0,0 +1,5 @@
+export interface TownHallEmployee {
+  id: number;
+  nom : string;
+  prenom: string;
+}
