417b8e53bcc5c74b783bda8f40d71cf17d35c6b0
Julien N'diaye <julienn.ndiaye59155@gmail.com>
2020-12-27 17:36:20 +0100

    refactoring

--- Frontend/src/app/shop-rating/shop-rating.component.ts
+++ Frontend/src/app/shop-rating/shop-rating.component.ts
@@ -15,7 +15,6 @@ import {Inhabitant} from "../../models/inhabitant.model";
 export class ShopRatingComponent implements OnInit {
 
   currentRate: number;
-  alreadyVoted = false;
   votersNumber: number;
   idShop: string;
   public inhabitant: Inhabitant;
@@ -25,33 +24,49 @@ export class ShopRatingComponent implements OnInit {
   inhabitantIsInsideShop: boolean;
 
   constructor(private shopService: ShopService, private inhabitantService: InhabitantService, public dialog: MatDialog) {
+    this.setUpShop();
+    this.initializeInhabitant();
+  }
+
+  setUpShop(){
     this.shopService.shopSelected$.subscribe((shop) =>{
       this.shop=shop;
       this.currentRate = shop.storeRating.averageRate;
+      this.controlShopRate();
       this.idShop = shop.id;
       this.testInsideShop();
     });
-    this.inhabitantService.inhabitant$.asObservable().subscribe(
-      (inhabitant) => {
-        this.inhabitant = inhabitant;
-        this.inhabitant = this.inhabitantService.currentInhabitant;
-      }
+  }
 
-    );
+  controlShopRate() {
     if (this.currentRate == undefined) {
       this.currentRate = 0;
     }
   }
 
-  achat() {
+  initializeInhabitant(){
+    this.inhabitantService.inhabitant$.asObservable().subscribe(
+      (inhabitant) => {
+        this.inhabitant = inhabitant;
+        this.inhabitant = this.inhabitantService.currentInhabitant;
+      }
+    );
+  }
+
+  openThanksDialog(){
     const dialogRef = this.dialog.open(ThanksComponent, {
       width: '25%',
       height: '10%',
     });
+  }
+
+  inhabitantControl(){
     if (this.shop.averagePresenceBeforePurchase == undefined) {
       this.shop.averagePresenceBeforePurchase = {numberOfPurchases: 0, numberOfPresence: 0};
     }
-    this.shop.averagePresenceBeforePurchase.numberOfPurchases = this.shop.averagePresenceBeforePurchase.numberOfPurchases + 1;
+  }
+
+  addInhabitantAttendanceToShop(){
     if (this.inhabitant.positions != undefined) {
       this.shop.averagePresenceBeforePurchase.numberOfPresence += this.inhabitant.positions
         .filter((position) =>
@@ -62,74 +77,75 @@ export class ShopRatingComponent implements OnInit {
       this.shop.averagePresenceBeforePurchase.numberOfPresence ++;
     }
 
-    this.addItemsToInhabitantAndManagePositions();
-
-    this.shopService.updateShop(this.shop);
-    this.inhabitantService.authenticateInhabitant(this.inhabitant.id).subscribe((inhabitant) => this.inhabitant = inhabitant);
+    this.shop.averagePresenceBeforePurchase.numberOfPurchases = this.shop.averagePresenceBeforePurchase.numberOfPurchases + 1;
   }
 
-  addItemsToInhabitantAndManagePositions(){
+  purchase() {
+    this.openThanksDialog();
+    this.inhabitantControl();
+    this.addInhabitantAttendanceToShop();
+    this.addItemsToInhabitant();
+    this.manageInhabitantAttendance();
+    this.inhabitantService.updateInhabitant(this.inhabitant);
+    this.shopService.updateShop(this.shop);
+    this.inhabitantService.authenticateInhabitant(this.inhabitant.id).subscribe((inhabitant) => this.inhabitant = inhabitant);
+    this.objectName = undefined;
+  }
+
+  addItemsToInhabitant(){
     if (this.inhabitant.objectPurchased == undefined){
       this.inhabitant.objectPurchased = [];
     }
-    const array = [];
-    array.push(this.objectName.toString());
-    array.push(this.idShop.toString());
+    const array = [];array.push(this.objectName.toString());array.push(this.idShop.toString());
     this.inhabitant.objectPurchased.push(array);
-    this.inhabitantService.updateInhabitant(this.inhabitant);
-    this.objectName = undefined;
-    const array2  = this.inhabitant.positions
+
+  }
+
+  manageInhabitantAttendance(){
+    this.inhabitant.positions = this.inhabitant.positions
       .filter((position) =>
         position[0] != this.inhabitant.longitude || position[1] != this.inhabitant.latitude);
-    this.inhabitantService.updateInhabitantPositions(array2);
   }
 
   ngOnInit(): void {
   }
 
   rateShop(){
-    if (this.inhabitantService.getShopIfRatedByAnInhabitant(Number(this.shopService.shopSelected.id)) == undefined) {
-      this.inhabitantService.currentInhabitant.shopRated.push(Number(this.shopService.shopSelected.id));
-      this.setRateWait();
-      if (this.inhabitantService.currentInhabitant.shopRated == undefined){
-        const array = [];
-        array.push(Number(this.shopService.shopSelected.id));
-        this.inhabitantService.updateShopRatedByInhabitant(array);
+    setTimeout(
+      () => {
+        if (this.inhabitantService.getShopIfNotAlreadyRatedByAnInhabitant(Number(this.shopService.shopSelected.id)) == undefined) {
+          this.inhabitantService.currentInhabitant.shopRated.push(Number(this.shopService.shopSelected.id));
+          this.setRateWait();
+          const array = this.inhabitantService.currentInhabitant.shopRated;
+          array.push(Number(this.shopService.shopSelected.id));
+          this.inhabitantService.updateShopRatedByInhabitant(array);
+        }
       }
-      else {
-        const array = this.inhabitantService.currentInhabitant.shopRated;
-        array.push(Number(this.shopService.shopSelected.id));
-        this.inhabitantService.updateShopRatedByInhabitant(array);
-      }
-    }
+      , 500);
   }
 
-  setShop(){
+  setShopRate() {
+    this.votersNumber = this.shopService.shopSelected.storeRating.voterNumber;
+    this.votersNumber++;
+
+    this.shopService.shopSelected.storeRating.averageRate = (this.shopService.shopSelected.storeRating.averageRate *
+      this.shopService.shopSelected.storeRating.voterNumber +
+      this.currentRate)
+      /
+      (this.votersNumber);
+
+    this.shopService.shopSelected.storeRating.voterNumber++;
+    this.shopService.updateShop(this.shopService.shopSelected);
+  }
+
+  getShopFromUrl(){
     this.shopService.getShopFromUrl(this.idShop);
   }
 
   setRateWait(){
     setTimeout(
-      () => this.setRate()
-      , 10);
-  }
-
-  setRate() {
-    if (!this.alreadyVoted) {
-      this.votersNumber = this.shopService.shopSelected.storeRating.voterNumber;
-      this.votersNumber++;
-      this.alreadyVoted = true;
-
-      this.shopService.shopSelected.storeRating.averageRate = (this.shopService.shopSelected.storeRating.averageRate *
-        this.shopService.shopSelected.storeRating.voterNumber +
-        this.currentRate)
-        /
-        (this.votersNumber);
-
-      this.shopService.shopSelected.storeRating.voterNumber++;
-      this.shopService.updateShop(this.shopService.shopSelected);
-      // créer un tableau dans user pour savoir s'il a déjà voté pour un magasin
-    }
+      () => this.setShopRate()
+      , 20);
   }
 
   private testInsideShop() {
