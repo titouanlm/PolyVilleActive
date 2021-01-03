fa5afcca86130af46a87ea0bfd93250d1d4473b4
abadina <50300952+abadina@users.noreply.github.com>
2021-01-03 17:23:48 +0100

    To show cultural actor events

--- Frontend/src/app/app.module.ts
+++ Frontend/src/app/app.module.ts
@@ -41,6 +41,7 @@ import { TownHallEmployeeHomeComponent } from './town-hall-employee-home/town-ha
 import { StoreItemsComponent } from './store-items/store-items.component';
 import { DialogAlertShopComponent } from './dialog-alert-shop/dialog-alert-shop.component';
 import { ItemStatisticsComponent } from './item-statistics/item-statistics.component';
+import {CulturalActorEventsComponent} from "./cultural-actor-events/cultural-actor-events.component";
 
 
 
@@ -76,6 +77,7 @@ import { ItemStatisticsComponent } from './item-statistics/item-statistics.compo
     StoreItemsComponent,
     DialogAlertShopComponent,
     ItemStatisticsComponent,
+    CulturalActorEventsComponent,
   ],
   imports: [
     BrowserModule,
@@ -97,6 +99,7 @@ import { ItemStatisticsComponent } from './item-statistics/item-statistics.compo
       {path: 'visitorinhabitant/citycenterinformation', component: CityCenterInformationComponent},
       {path: 'cultural-actor', component: CulturalActorHomeComponent},
       {path: 'cultural-actor/blockly', component: CulturalActorBlocksComponent},
+      {path: 'cultural-actor/events', component: CulturalActorEventsComponent},
       {path: 'town-hall-employee', component: TownHallEmployeeHomeComponent},
       {path: 'town-hall-employee/blockly', component: TownHallEmployeeBlocksComponent},
       { path: '**', redirectTo: '' }
---
+++ Frontend/src/app/cultural-actor-events/cultural-actor-events.component.ts
@@ -0,0 +1,30 @@
+import { Component, OnInit } from '@angular/core';
+import {CulturalEvent} from "../../models/event.model";
+import {EventService} from "../../services/event.service";
+import {CulturalActorService} from "../../services/culturalActor.service";
+import {CulturalActor} from "../../models/culturalActor.model";
+
+
+@Component({
+  selector: 'app-cultural-actor-events',
+  templateUrl: './cultural-actor-events.component.html',
+  styleUrls: ['./cultural-actor-events.component.scss']
+})
+export class CulturalActorEventsComponent implements OnInit {
+  public culturalActor:CulturalActor;
+  public eventList: CulturalEvent[];
+
+  constructor(public eventService: EventService, public culturalActorService: CulturalActorService) {
+    this.culturalActorService.getCulturalEvents(String(this.culturalActor.id));
+    this.culturalActorService.cevents$.subscribe((events)=>this.eventList=events);
+
+  }
+
+  ngOnInit(): void {
+  }
+/*
+  setShop(number){
+    this.shopService.getShopFromUrl(this.shopList[number].id);
+  }
+*/
+}
