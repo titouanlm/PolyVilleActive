cc2872ca34bd7712526bc60818bdf3cf7ebe9847
titouanlm <titouan.le-mao@etu.unice.fr>
2021-01-06 15:23:48 +0100

    Début simulateur parking #59

--- Frontend/src/app/parking/parking.component.ts
+++ Frontend/src/app/parking/parking.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {nameAvailabilityType, Shop} from "../../models/shop.model";
+import {ShopService} from "../../services/shop.service";
 
 @Component({
   selector: 'app-parking',
@@ -7,7 +9,24 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ParkingComponent implements OnInit {
 
-  constructor() { }
+  public places : nameAvailabilityType[] = [];
+  public shops : Shop[];
+  constructor(public shopService : ShopService) {
+    this.shopService.getShopsFromUrl();
+    let test = true;
+    this.shopService.shops$.subscribe((shops) =>{
+      this.shops = shops;
+      if(this.shops && test){
+        this.shops.forEach((shop) =>{
+          if(shop.parkingSpace){
+            this.places = this.places.concat(shop.parkingSpace.places);
+          }
+        })
+        test =false;
+      }
+    })
+
+  }
 
   ngOnInit(): void {
   }
